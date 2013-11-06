648bf587e16e39d044813527968a6cc248e262eb
Al Zimmerman <auraelius@gmail.com>
2013-11-06 13:28:56 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quizzler::Application.config.secret_key_base = 'af873cee288d1b6c07438c4b1ec3a05841999e1be3022796bebded28768e22d3a1fb1edfa7803327d3f92cf07fa75f7f634f61b336be13626f2b779af69607fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quizzler::Application.config.session_store :cookie_store, key: '_quizzler_session'
